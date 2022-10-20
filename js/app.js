37c4d47bb7206ace5ea906eac1d5e36bc77fd715
Jesse Whitehead <54863103+Jessbob@users.noreply.github.com>
2022-10-20 15:15:42 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,8 +28,8 @@ fetch(endpoint, requestOptions)
       if (response.ok) document.getElementById("result-text").innerText =
         "Thank you for inquirering about our protection plan. We will reach out soon with details.";
     })
-    .catch((error) => {
+  /*   .catch((error) => {
       document.getElementById("result-text").innerText =
         "An unkown error occured. But did it Really?";
-    });
+    }); */
 });
